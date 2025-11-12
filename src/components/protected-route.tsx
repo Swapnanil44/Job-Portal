09f07b7a2f25317c41de9b0e5810a13201fdffa5
Swapnanil Majumdar <adioticswapnanil@gmail.com>
2025-11-12 20:12:56 +0530

    add Onboarding

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -4,10 +4,14 @@ import { Navigate, useLocation } from "react-router";
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const {isSignedIn, isLoaded, user} = useUser();
-  const {pathName} = useLocation();
+  const {pathname} = useLocation();
   
   if(isLoaded && !isSignedIn && isSignedIn !== undefined){
     return <Navigate to="/?sign-in=true"/>
+  }
+
+  if(user !== undefined && !user.unsafeMetadata.role && pathname !== '/onboarding' ){
+    return <Navigate to="/onboarding"/>
   }
   return children
 }
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,20 +1,12 @@
-
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-const supabaseClient = async(supabaseAccessToken: string)=>{
-    const supabase = createClient(supabaseAccessToken, supabaseKey, {
-        global:{
-            headers:{
-                Authorization: `Bearer ${supabaseAccessToken}`
-            }
-        }
-    })
-    return supabase;
-}
-export default supabase
-        
+const supabaseClient = async (supabaseAccessToken: string) => {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: { headers: { Authorization: `Bearer ${supabaseAccessToken}` } },
+  });
+  return supabase;
+};
+export default supabaseClient;
