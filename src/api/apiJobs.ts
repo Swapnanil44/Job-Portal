ba69392061a5edd0c7daa2665820ee38ab9822bc
Swapnanil Majumdar <adioticswapnanil@gmail.com>
2025-11-16 18:52:15 +0530

    add jobs listing page

--- src/api/apiJobs.ts
+++ src/api/apiJobs.ts
@@ -1,16 +1,31 @@
 import supabaseClient from "@/utils/supabase";
 
-export async function getJobs(token: string){
-    const supabase = await supabaseClient(token);
+export async function getJobs(
+  token: string,
+  {
+    location,
+    company_id,
+    searchQuery,
+  }: { location?: string; company_id?: string; searchQuery?: string }
+) {
+  const supabase = await supabaseClient(token);
 
-    let query = supabase.from("jobs").select("*");
+  let query = supabase.from("jobs").select("*, company:companies(name,logo_url), saved: saved_jobs(id)");
+  if(location){
+    query = query.eq("location",location)
+  }
+  if(company_id){
+    query = query.eq("company_id",company_id);
+  }
+  if(searchQuery){
+    query = query.ilike("title",`%${searchQuery}%`)
+  }
+  const { data, error } = await query;
 
-    const {data, error} = await query;
+  if (error) {
+    console.log("Error fetching jobs: ", error);
+    throw new Error(error.message);
+  }
 
-    if(error){
-        console.log("Error fetching jobs: ", error);
-        throw new Error(error.message);
-    }
-
-    return data;
+  return data;
 }
--- src/pages/job-listing.tsx
+++ src/pages/job-listing.tsx
@@ -1,31 +1,72 @@
 import { getJobs } from "@/api/apiJobs";
+import JobCard from "@/components/job-card";
 import useFetch from "@/hooks/use-fetch";
-import { useSession, useUser } from "@clerk/clerk-react";
-import { useEffect, useMemo } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect, useMemo, useState } from "react";
+import type { DiscoverBehavior } from "react-router";
+import { BarLoader } from "react-spinners";
 
 function JobListing() {
-  const fetchOptions = useMemo(() => ({}), []);
-  
+  // location,
+  //   company_id,
+  //   searchQuery,
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const fetchOptions = useMemo(
+    () => ({ location, company_id, searchQuery }),
+    [location, company_id, searchQuery]
+  );
 
   const {
     fn: fnJobs,
-    data: dataJobs,
+    data: jobs,
     loading: loadingJobs,
-    error
-  } = useFetch(getJobs,fetchOptions);
+    error,
+  } = useFetch(getJobs, fetchOptions);
 
-  console.log(loadingJobs)
-  console.log(dataJobs);
-  console.log(error)
+  console.log(loadingJobs);
+  console.log(jobs);
+  console.log(error);
   const { isLoaded, isSignedIn } = useUser();
-  
+
   useEffect(() => {
     if (isLoaded && isSignedIn) {
       fnJobs();
     }
-  }, [isLoaded, isSignedIn,fnJobs]);
+  }, [isLoaded, isSignedIn, fnJobs]);
 
-  return <div>JobListing</div>;
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+      {loadingJobs && (
+        <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+      )}
+      {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs.length > 0 ? (
+            jobs.map((job: any) => {
+              return (
+                <JobCard
+                  key={job.id}
+                  job={job}
+                  savedInit={job?.saved?.length > 0}
+                />
+              );
+            })
+          ) : (
+            <div> No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default JobListing;
